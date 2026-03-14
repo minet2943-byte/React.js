a5de148245555bd8178a17680051ffee57d51b1e
Neth <minet2943@gmail.com>
2026-03-14 18:26:35 +0700

    first project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+function App() {
+
+// const state = useState(0);
+// const count =state[0];
+// const setCount = state[1];
+
+const [count , setCount] = useState(0);
+
+  return (
+    <div>
+          <h1 style={{color:"blue"}}>Count : {count}</h1>
+          <button style={{padding:"10px"}} onClick={() => setCount (count + 1)}>increment</button>
+          <button style={{padding:"10px", marginLeft:"20px"}} onClick={() => setCount(count-1)}>Decrement</button>
+
+    </div>
+  )
+}
+
+export default App
